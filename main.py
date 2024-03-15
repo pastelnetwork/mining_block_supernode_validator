3706cee291dd8627c79eafa56ebb5e848d7c8cb2
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2024-03-15 00:36:10 -0400

    Changed config file name

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ app.add_middleware(
     expose_headers=["Authorization"]
 )
 
-filepath_to_pastelid_secrets = 'pastelids_for_devnet_sns.yml'
+filepath_to_pastelid_secrets = 'pastelids_for_sns.yml'
 
 async def load_yaml(file_path):
     async with aiofiles.open(file_path, 'r') as file:
